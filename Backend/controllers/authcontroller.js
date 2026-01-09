b89343bb28dd08fb2702e1dd42e9733e2a59702f
Charan951 <charanteja@speshway.com>
2026-01-09 12:11:27 +0530

    type3

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -13,7 +13,8 @@ const registerUser=async(req,res)=>{
          const hashedPassword=await bcrypt.hash(password,10);
          const user=new User({name,email,password:hashedPassword});
          await user.save();
-         res.status(201).json({message:'User registered successfully'});
+         res.status(201).json({user,message:'User registered successfully'});
+         
     }catch(error){
         res.status(500).json({message:'Server error'});
     }
